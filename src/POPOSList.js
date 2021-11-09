f76558a29a3e12fd67a4908c4d1918a7b80b723a
gizellzamora <gizell.zamoraregalado@gmail.com>
2021-11-09 12:23:06 -0800

    chapter 1-3

---
+++ src/POPOSList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import POPOSSpace from './POPOSSpace';
+import './POPOSList.css';
+
+function POPOSList() {
+  return (
+    <div className="POPOSList">
+      <POPOSSpace
+        name="50 California Street"
+        address="50 California St."
+        image="50-california-st.jpg"
+      />
+      <POPOSSpace />
+      <POPOSSpace />
+      <POPOSSpace />
+    </div>
+  )
+}
+
+export default POPOSList
---
+++ src/Title.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from './logo.svg';
+import './App.css';
+import './Title.css';
+
+function Title() {
+  return (
+    <div className="Title">
+        <h1>SFPOPOS</h1>
+  			<div className="Title-Subtitle">San Franciscos Privately Owned Public Spaces</div>
+      </div>
+    )
+  }
+
+export default Title
